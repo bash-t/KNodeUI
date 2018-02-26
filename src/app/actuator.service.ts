a96101645f90e5385ef3e12730817c3de6bc35d0
Bastian Pawlik <bashtcooper@googlemail.com>
2018-02-26 19:00:36 +0100

    Message Service added
    added Component and Services for displaying messages.
    
    Wrapping actuators in container class to fully use bootstrap layout.

--- src/app/actuator.service.ts
+++ src/app/actuator.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Actuator } from './actuator';
 import { ACTUATORS } from './config/actuator.config';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class ActuatorService {
 
-  constructor() { }
+constructor(private messageService: MessageService) { }
 
   getActuators(): Actuator[] {
+    this.messageService.add('all actuators loaded');
     return ACTUATORS;
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,32 @@ import { NgPipesModule } from 'ng-pipes';
 
 //import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 
 import { AppComponent } from './app.component';
 import { ActuatorsComponent } from './actuators/actuators.component';
 import { ActuatorService } from './actuator.service';
 import { MenuComponent } from './menu/menu.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ActuatorsComponent,
-    MenuComponent
+    MenuComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     NgPipesModule,
-    CollapseModule
+    CollapseModule,
+    AlertModule.forRoot()
     //  BsDropdownModule.forRoot()
   ],
-  providers: [ActuatorService],
+  providers: [ActuatorService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
